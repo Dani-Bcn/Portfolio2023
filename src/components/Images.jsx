44fcd0c1506a721df58d98ffdb3906d1c5475aec
Dani-Bcn <nneodani@gmail.com>
2023-10-01 23:43:04 +0200

    Test effects images projects

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,8 +1,7 @@
-import image1 from '../img/img-poke.png'
-import image2 from '../img/img-movies.png'
-import image3 from '../img/img-kids.png'
-import image4 from '../img/Photo.jpeg'
-
+import image1 from "../img/img-poke.jpg"
+import image2 from "../img/img-movies.jpg"
+import image3 from "../img/img-kids.jpg"
+import image4 from "../img/photo.jpeg"
 const images =[
     image1,
     image2,
